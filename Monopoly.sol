570d70b76682a12f4a6d99a581a912d05766da01
Adam Koza <adam.koza@mail.utoronto.ca>
2018-10-14 20:27:03 -0400

    Start adding property info.

--- Monopoly.sol
+++ Monopoly.sol
@@ -30,21 +30,21 @@ contract Monopoly {
     struct Property {
         string name;
         string color;
-        bool owned;
-        uint owner;
-        bool morgaged;
         uint cost;
         uint rent;
-        uint houses;
-        uint hotel;
-        uint house_cost;
-        uint hotel_cost;
         uint oneHouse_rent;
         uint twoHouse_rent;
         uint threeHouse_rent;
         uint fourHouse_rent;
         uint hotel_rent;
         uint mortgage_value;
+        uint house_cost;
+        uint hotel_cost;
+        bool owned;
+        uint owner;
+        bool morgaged;
+        uint houses;
+        bool hotel;
     }
     
     struct Utility {
@@ -104,13 +104,33 @@ contract Monopoly {
     constructor() public {
         
         // Build Board.
+        // reference: http://monopoly.wikia.com/wiki/Property
+        // https://vignette.wikia.nocookie.net/monopoly/images/2/22/Monopoly-Board.jpg/revision/latest?cb=20141114170252
         board[0] = "Go";
-        board[1] = "Property"; // propertyInfo[1] = Property({name: "lol"});
+        board[1] = "Property";
+        propertyInfo[1] = Property({name: "Mediterranean Avenue",
+            color: "Dark Purple", cost: 60, rent: 2, oneHouse_rent: 10,
+            twoHouse_rent: 30, threeHouse_rent: 90, fourHouse_rent: 160,
+            hotel_rent: 250, mortgage_value: 30, house_cost: 50,
+            hotel_cost: 50, owned: false, owner: 0x0, morgaged: false,
+            houses: 0, hotel: false});
         board[2] = "Community Chest";
         board[3] = "Property";
+        propertyInfo[3] = Property({name: "Baltic Avenue",
+            color: "Dark Purple", cost: 60, rent: 4, oneHouse_rent: 20,
+            twoHouse_rent: 60, threeHouse_rent: 180, fourHouse_rent: 320,
+            hotel_rent: 450, mortgage_value: 30, house_cost: 50,
+            hotel_cost: 50, owned: false, owner: 0x0, morgaged: false,
+            houses: 0, hotel: false});
         board[4] = "Tax";
         board[5] = "Railroad";
         board[6] = "Property";
+        propertyInfo[6] = Property({name: "Oriental Avenue",
+            color: "Light Blue", cost: 60, rent: 4, oneHouse_rent: 20,
+            twoHouse_rent: 60, threeHouse_rent: 180, fourHouse_rent: 320,
+            hotel_rent: 450, mortgage_value: 30, house_cost: 50,
+            hotel_cost: 50, owned: false, owner: 0x0, morgaged: false,
+            houses: 0, hotel: false});
         board[7] = "Chance";
         board[8] = "Property";
         board[9] = "Property";
