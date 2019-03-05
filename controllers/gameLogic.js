04436a2d48415230c6d87f134655f3aedfff797d
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-03-05 02:53:10 -0500

    Add initial Chance & Community Chest deck shuffle.

--- controllers/gameLogic.js
+++ controllers/gameLogic.js
@@ -9,7 +9,7 @@ function roll(inJail, doublesCount, currentPosition) {
     oldPosition = currentPosition;
     currentPosition = ((dice1 + dice2) + currentPosition) % 40;
 
-    // Roll Doubles.
+    // Roll doubles.
     if (dice1 == dice2) {
         // Roll out of jail.
         if (injail) {
@@ -24,7 +24,7 @@ function roll(inJail, doublesCount, currentPosition) {
         // Reset doublesCount.
         doublesCount = 0;
     }
-    // Stay in Jail.
+    // Stay in jail.
     if (inJail) {
         return [true, 0, 10, dice1, dice2, false, true];
     }
@@ -41,3 +41,96 @@ function roll(inJail, doublesCount, currentPosition) {
         return [false, doublesCount, currentPosition, dice1, dice2, false, false];
     }
 }
+
+// Shuffle Community Chest and Chance Decks.
+// Return Int[16] CardIndexs
+//
+function shuffleDeck() {
+    deck = [];
+    while (deck.length < 16) {
+        found = false;
+        randIndex = Math.floor(Math.random() * 16);
+        for (i = 0; i < deck.length; i++) {
+            if (deck[i] == randIndex) {
+                found = true;
+            }
+        }
+        if (found == false) {
+            deck.push(randIndex);
+        }
+    }
+    return deck;
+}
+
+// Pull from Chance deck.
+// Return [String Event, Int NewPlayerBalance, Int NewPosition, Int NewMiddlePot, 
+//          Bool JailFree, Bool InJail, Bool TurnOver, Bool PayPlayers, Int RentMultiplier]
+//
+function pullChance(deck, drawCount, player, playerBalance, currentPosition, middlePot, playerCount) {
+    index = drawCount % 16;
+    card = deck[index];
+    if (card == 0) { return ['Advance to "Go", collect $200', playerBalance + 200, 0, middlePot, false, false, true, false, 1]; }
+    if (card == 1) {
+        if (24 < currentPosition) { playerBalance += 200; }
+        return ['Advance to Illinois Avenue. If you pass Go, collect $200.', playerBalance, 24, middlePot, false, false, false, false, 1];
+    }
+    if (card == 2) {
+        if (11 < currentPosition) { playerBalance += 200; }
+        return ['Advance to St. Charles Place. If you pass Go, collect $200.', playerBalance, 11, middlePot, false, false, false, false, 1];
+    }
+    // Come back to this.
+    if (card == 3) {
+        return ['Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total 10 times the amount thrown.'];
+    }
+    // Come back to this.
+    if (card == 4 || card == 5) {
+        return ['Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.'];
+    }
+    if (card == 6) { return ['Bank pays you dividend of $50.', playerBalance += 50, currentPosition, middlePot, false, false, true, false, 1]; }
+    if (card == 7) { return ['Get out of Jail Free. This card may be kept until needed, or traded/sold.', playerBalance, currentPosition, middlePot, true, false, true, false, 1]; }
+    if (card == 8) {
+        if ((currentPosition - 3) < 0) { currentPosition = 39 + (currentPosition - 3); } else { currentPosition -= 3; }
+        return ['Go back three spaces.', playerBalance, currentPosition, middlePot, false, false, true, false, 1];
+    }
+    if (card == 9) { return ['Go to Jail. Go directly to Jail. Do not pass GO, do not collect $200.', playerBalance, 10, middlePot, false, true, true, false, 1]; }
+    if (card == 10) { let repair = calculateRepair(player); return ['Make general repairs on all your property: For each house pay $25, For each hotel pay $100.', playerBalance -= repair, currentPosition, middlePot += repair, false, false, true, false, 1]; }
+    if (card == 11) { return ['Pay poor tax of $15.', playerBalance -= 15, currentPosition, middlePot += 15, false, false, true, false, 1]; }
+    if (card == 12) {
+        if (5 < currentPosition) { playerBalance += 200; }
+        return ['Take a trip to Reading Railroad. If you pass Go, collect $200.', playerBalance, 5, middlePot, false, false, false, false, 1];
+    }
+    if (card == 13) { return ['Take a walk on the Boardwalk. Advance token to Boardwalk.', playerBalance, 39, middlePot, false, false, false, false, 1]; }
+    if (card == 14) { return ['You have been elected Chairman of the Board. Pay each player $50.', playerBalance -= (playerCount - 1) * 50, currentPosition, middlePot, false, false, true, true, 1]; }
+    if (card == 15) { return ['Your building and loan matures. Receive $150.', playerBalance += 150, currentPosition, middlePot, false, false, true, false, 1]; }
+
+}
+
+// Calculate nearest Utility.
+// Return Int NewPosition.
+//
+function nearestUtility(currentPosition) {
+
+}
+
+// Calculate nearest Rail Road.
+// Return Int NewPosition.
+//
+function nearestRailRoad(currentPosition) {
+
+}
+
+// Calculate property repair chance card.
+// Return Int RepairCost.
+//
+function calculateRepair(player) {
+    houses = 0;
+    hotels = 0;
+    // for each property owned by player {
+    // houses += property.houses * 25;
+    // hotels += property.hotel * 100;
+    // }
+    return houses + hotels;
+}
+
+
+
