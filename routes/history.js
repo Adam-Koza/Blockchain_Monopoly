fbfec13be91c7e64b0c5a2e84ab250623181f4cc
yanesh <yaneshcode@gmail.com>
2019-03-08 21:04:03 -0500

    history controller

---
+++ routes/history.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var historyController = require('../controllers/historyController');
+
+// Display all game history
+router.get('/index', historyController.index);
+
+// Display game history by id
+router.get('/index/:id', historyController.show);
+
+// Create new game history page
+// creating new game history should be done automatically
+// no reason to call this route. testing only
+router.get('/new', historyController.new);
+
+// Saving a new game history
+router.post('/save', historyController.save);
+
+// Update an existing game history
+router.get('/update/:id', historyController.showUpdate);
+router.post('/update/:id', historyController.update);
+
+// Delete an existing game history
+router.get('/delete/:id', historyController.delete);
+
+module.exports = router;
